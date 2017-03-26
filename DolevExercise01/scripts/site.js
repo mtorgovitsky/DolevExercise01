2eeeae94d4fb7e2a782ff17d91aebe913e227285
Michael Torgovitsky <m.torgovitsky@gmail.com>
2017-03-26 14:52:41 +0300

    Final version of excersice

---
+++ DolevExercise01/scripts/site.js
@@ -0,0 +1,16 @@
+﻿function passwordsMatch() {
+    var password = document.getElementById("password");
+    var confirm = document.getElementById("confirm");
+    var matchText = document.getElementById("passwordsMatch");
+    if (password.value !== confirm.value) {
+        password.style.borderColor = "red";
+        confirm.style.borderColor = "red";
+        matchText.style.display = "block";
+        matchText.innerHTML = "Passwords Don't Match!";
+    }
+    else {
+        password.style.borderColor = "inherit";
+        confirm.style.borderColor = "inherit";
+        matchText.style.display = "none";
+    }
+}
